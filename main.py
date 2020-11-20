e12802692fc82df2e6501c697672085e35291ffc
Niranjan B <niranjannb7777@gmail.com>
2020-11-20 20:53:08 +0530

    venv

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
+import io
 
-import cv2
 import discord
 import requests
+from PIL import Image
 
 intents = discord.Intents.default()
 intents.members = True
@@ -15,36 +15,34 @@ async def on_member_join(member):
     r = requests.get(member.avatar_url, stream=True,
                      headers={'User-agent': 'Mozilla/5.0'})
     print("Downloading Image")
-    if r.status_code == 200:
-        with open("img.png", 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-
     try:
-        img = cv2.imread('img.png')
-        img = cv2.resize(img, (130, 130), interpolation=cv2.INTER_AREA)
-        h, w = 130, 130  # img.shape
+        if r.status_code == 200:
+            img = Image.open(io.BytesIO(r.content))
+            img = img.resize((130, 130))
+            img = img.convert("RGBA")
+            h, w = 130, 130  # img.shape
 
-        # load background image as grayscale
+            # load background image as grayscale
 
-        back = cv2.imread('background.png')
-        hh, ww = 480, 720  # back.shape
-        back = cv2.resize(back, (ww, hh), interpolation=cv2.INTER_AREA)
+            back = Image.open('background.png')
+            hh, ww = 480, 720  # back.shape
+            back = back.resize((ww, hh))
 
+            # compute xoff and yoff for placement of upper left corner of resized image
+            yoff = round((hh - h - 140) / 2)
+            xoff = round((ww - w) / 2)
 
-        # compute xoff and yoff for placement of upper left corner of resized image
-        yoff = round((hh - h - 140) / 2)
-        xoff = round((ww - w) / 2)
+            # use numpy indexing to place the resized image in the center of background image
+            result = back.copy()
+            result.paste(img,box=(xoff,yoff,xoff+w,yoff+h),mask=img)
+            # result[yoff:yoff + h, xoff:xoff + w] = img
 
-        # use numpy indexing to place the resized image in the center of background image
-        result = back.copy()
-        result[yoff:yoff + h, xoff:xoff + w] = img
-
-        # save resulting centered image
-        filename = 'welcome.png'
-        cv2.imwrite(filename, result)
-        print("Image Created")
-    except:
+            # save resulting centered image
+            filename = 'welcome.png'
+            result.convert(back.mode).save(filename)
+            print("Image Created")
+    except Exception as e:
+        print(e)
         filename = None
 
     if filename:
